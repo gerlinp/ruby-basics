3dd5d46da613e4b864faaeade63ecb84629a6eac
Gerlin <gerlin.pierrelouis@gmail.com>
2022-06-11 13:22:53 -0400

    refactoring

--- temp.rb
+++ temp.rb
@@ -1,14 +1,9 @@
-def check_speed(speed)
-    if speed > 55 
-      message = "too fast"
-    end
-    if speed < 55 
-      message = "too slow"
-    end
-    if speed == 55
-      message = "speed ok"
-    end
-      p message
-  end
-  
-  p check_speed(45)
+if_condition = true
+elsif_condition = true
+if if_condition
+    puts "if clause"
+elsif elsif_condition
+    puts "elseif clause"
+else
+    puts "else clause"
+end
